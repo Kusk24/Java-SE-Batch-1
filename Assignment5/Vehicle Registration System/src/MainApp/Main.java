9fde14a8c0b0a718fad77730e4fa09724240a733
Kusk24 <winyumg2003a@gmail.com>
2024-08-31 19:32:32 +0700

    Modified Assignment5

--- Assignment5/Vehicle Registration System/src/MainApp/Main.java
+++ Assignment5/Vehicle Registration System/src/MainApp/Main.java
@@ -1,15 +1,39 @@
 package MainApp;
 
+import Model.Vehicle;
+import Service.CarRegisterService;
+import Service.MotorcycleRegisterService;
+import Service.TruckRegisterService;
 import Service.VehicleRegisterService;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
+        String type;
+        String flag;
         VehicleRegisterService service = new VehicleRegisterService();
 
-        service.getVehicleInfo();
-
+        do {
+            System.out.print("Enter Vehicle Type (Car/Truck/Motorcycle): ");
+            type = br.readLine();
+            if (type.equalsIgnoreCase("Car")) {
+                service = new CarRegisterService();
+            } else if (type.equalsIgnoreCase("Truck")) {
+                service = new TruckRegisterService();
+            } else if (type.equalsIgnoreCase("Motorcycle")) {
+                service = new MotorcycleRegisterService();
+            }
+            service.getCommonInfo();
+            service.getRegisterInfo();
+            System.out.print("Do you want to add another vehicle: (yes/no): ");
+            flag = br.readLine();
+            System.out.println(Vehicle.getVehicleCount());
+        } while(flag.equalsIgnoreCase("Yes"));;
         service.display();
     }
 }
